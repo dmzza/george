bcac7e6b1237b9a2680a9c25bcb5126878a9be38
David Mazza <github@dmzza.com>
2013-06-05 18:21:29 -0400

    Commitment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+George::Application.config.secret_token = 'bc9fb0d8186a9e1d4ac344fee01425a88aa1bcce7f924f9efb525d52c10fbe46cbfabecbc01b2009f17c4e0961a2c02df0e2d8bf5816d9f67981f911c927a7d1'
