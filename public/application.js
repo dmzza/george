bbab7ec584ed5af64b4f57f3de9b09a440c25731
David Mazza <github@dmzza.com>
2013-10-14 12:58:34 -0400

    Extracted drawing to a render function

--- public/application.js
+++ public/application.js
@@ -1,19 +1,48 @@
+var requestAnimationFrame =  
+  window.requestAnimationFrame ||
+  window.webkitRequestAnimationFrame ||
+  window.mozRequestAnimationFrame ||
+  window.msRequestAnimationFrame ||
+  window.oRequestAnimationFrame ||
+  function(callback) {
+    return setTimeout(callback, 1);
+  };
+
+
+
 $(function() {
-canvas = document.getElementById("starry-night");
-sky = canvas.getContext("2d");
-HEIGHT = canvas.clientHeight;
-WIDTH = canvas.clientWidth;
-MAX_RADIUS = 4;
+  var canvas = document.getElementById("starry-night");
+  var sky = canvas.getContext("2d");
+  var stars = new Array(500);
+  HEIGHT = canvas.clientHeight;
+  WIDTH = canvas.clientWidth;
+  MAX_RADIUS = 4;
+  
+  for (var i = stars.length - 1; i >= 0; i--) {
+    x = Math.floor(Math.random()*WIDTH);
+    y = Math.floor(Math.random()*HEIGHT);
+    radius = Math.floor(Math.random()*MAX_RADIUS)+1;
+    stars[i] = {
+      'x': x,
+      'y': y,
+      'radius': radius
+    };
+  };
 
-sky.beginPath();
-stars = new Array(500);
-for (var i = stars.length - 1; i >= 0; i--) {
-  x = Math.floor(Math.random()*WIDTH);
-  y = Math.floor(Math.random()*HEIGHT);
-  radius = Math.floor(Math.random()*MAX_RADIUS)+1;
-  stars[i] = sky.arc(x,y,radius,0,2*Math.PI);
-};
-sky.fillStyle="#EEE";
-sky.fill();
+  var render = function() {
+    sky.clearRect(0, 0, WIDTH, HEIGHT);
+    sky.beginPath();
 
+    for (var i = stars.length - 1; i >= 0; i--) {
+      star = stars[i];
+
+      sky.arc(star.x, star.y, star.radius, 0, 2*Math.PI);
+    }
+
+    sky.fillStyle="#EEE";
+    sky.fill();
+  };
+
+  render();
 });
+
