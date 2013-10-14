011c7dff3c388d6270b5cfc2b0cc2f188faf8499
David Mazza <github@dmzza.com>
2013-10-14 12:36:13 -0400

    Starry Night replaces Wander

--- public/application.js
+++ public/application.js
@@ -1,88 +1,19 @@
+$(function() {
+canvas = document.getElementById("starry-night");
+sky = canvas.getContext("2d");
+HEIGHT = canvas.clientHeight;
+WIDTH = canvas.clientWidth;
+MAX_RADIUS = 4;
 
-var map;
-function initialize() {
-  var mapOptions = {
-    zoom: 15,
-    center: new google.maps.LatLng(40.707621,-73.921831),
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    panControl: false,
-    scrollwheel: false,
-    streetViewControl: false,
-  };
-  map = new google.maps.Map(document.getElementById('map'),
-      mapOptions);
-  google.maps.visualRefresh = true
-  transitLayer = new google.maps.TransitLayer();
-	transitLayer.setMap(map);
+sky.beginPath();
+stars = new Array(500);
+for (var i = stars.length - 1; i >= 0; i--) {
+  x = Math.floor(Math.random()*WIDTH);
+  y = Math.floor(Math.random()*HEIGHT);
+  radius = Math.floor(Math.random()*MAX_RADIUS)+1;
+  stars[i] = sky.arc(x,y,radius,0,2*Math.PI);
+};
+sky.fillStyle="#EEE";
+sky.fill();
 
-	var styles = [
-  {
-    stylers: [
-    ]
-  },{
-    featureType: "road",
-    elementType: "geometry",
-    stylers: [
-      { color: "#ffe867" },
-      { visibility: "simplified" }
-    ]
-  },{
-    featureType: "road.local",
-    elementType: "geometry",
-    stylers: [
-      { color: "#ffffff" },
-      { visibility: "simplified" }
-    ]
-  },{
-    featureType: "road",
-    elementType: "labels",
-    stylers: [
-      { visibility: "simplified" }
-    ]
-  },{
-    featureType: "landscape",
-    elementType: "geometry.fill",
-    stylers: [
-      { color: "#F4F4F4" }
-    ]
-  },{
-    featureType: "poi",
-    elementType: "label",
-    stylers: [
-      { visibility: "off" }
-    ]
-  },{
-    featureType: "poi.park",
-    elementType: "geometry",
-    stylers: [
-      { visibility: "on" },
-      { color: "#cdfbe1" }
-    ]
-  },{
-    featureType: "water",
-    elementType: "geometry.fill",
-    stylers: [
-      { color: "#bdf59e" }
-    ]
-  },{
-    featureType: "administrative.neighborhood",
-    elementType: "labels.text.fill",
-    stylers: [
-      { visibility: "on" },
-      { weight: 4 },
-      { color: "#222241" }
-    ]
-  },
-
-  ];
-
-  map.setOptions({styles: styles});
-        
-  var marker = new google.maps.Marker({
-      position: new google.maps.LatLng(40.707621,-73.921831),
-      map: map
-    });
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
-
+});
